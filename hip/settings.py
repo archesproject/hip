4a5edded21cf20f6304115c7c43bd0bec3ca9bb5
Alexei Peters <apeters@fargeo.com>
2014-09-30 23:25:48 -0700

    clean up settings, add skeleton setup.py

--- hip/settings.py
+++ hip/settings.py
@@ -1,26 +1,14 @@
-import sys
 import os
 import inspect
-
-try:
-    from arches.settings import *
-except ImportError:
-    pass
-
-LOAD_TEST_DATA = True
+from arches.settings import *
 
 PACKAGE_ROOT = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-PACKAGE = PACKAGE_ROOT.split(os.sep)[-1]
-DATABASES['default']['NAME'] = 'arches_%s' % (PACKAGE)
-
-SITE_URL = 'http://localhost:8000',
-INSTALLED_APPS = INSTALLED_APPS + (PACKAGE,)
-STATICFILES_DIRS = (os.path.join(ROOT_DIR, 'media'),) + STATICFILES_DIRS
-TEMPLATE_DIRS = (os.path.join(ROOT_DIR, 'templates'),os.path.join(ROOT_DIR, 'templatetags')) + TEMPLATE_DIRS
-LOGIN_URL = '/Arches/'
-DATA_CONCEPT_SCHEME = 'CA DOT'
-ENTITY_MODEL = {'default': 'arches.app.packages.%s.models.entity.Entity' % PACKAGE, 'project': 'arches.app.packages.%s.views.view_models.project.Project' % PACKAGE}
-HIP_TEST = 'so cool'
+PACKAGE_NAME = PACKAGE_ROOT.split(os.sep)[-1]
+DATABASES['default']['NAME'] = 'arches_%s' % (PACKAGE_NAME)
+INSTALLED_APPS = INSTALLED_APPS + (PACKAGE_NAME,)
+STATICFILES_DIRS = (os.path.join(PACKAGE_ROOT, 'media'),) + STATICFILES_DIRS
+TEMPLATE_DIRS = (os.path.join(PACKAGE_ROOT, 'templates'),os.path.join(PACKAGE_ROOT, 'templatetags')) + TEMPLATE_DIRS
+ENTITY_MODEL = {'default': 'models.entity.Entity'}
 
 try:
     from settings_local import *
---
+++ setup.py
@@ -0,0 +1,44 @@
+import os
+
+def install(rootpath):
+    trucate_db()
+    delete_index(index='concept')
+
+    load_resource_graphs()
+    load_authority_files()
+    load_map_layers()
+
+
+def export_data():
+    pass
+
+def import_data():
+    pass
+
+def trucate_db():
+    os.system('python manage.py packages --operation setup_db')
+    pass
+
+def load_resource_graphs():
+    pass
+
+def load_authority_files():
+    pass
+
+def load_map_layers():
+    pass
+
+def delete_index(index=None):
+    pass
+
+def install_dependencies():
+    pass
+
+def build_permissions():
+    pass
+
+def load_users():
+    pass
+
+def load_test_data():
+    pass
