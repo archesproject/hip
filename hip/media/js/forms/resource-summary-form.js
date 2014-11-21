053120d649248d27a1789344b439f263e37d040e
Rob Gaston <rgaston@fargeo.com>
2014-11-21 14:05:07 -0800

    updating for base form refactor

--- hip/media/js/forms/resource-summary-form.js
+++ hip/media/js/forms/resource-summary-form.js
@@ -10,22 +10,16 @@ define(['jquery', 'backbone', 'knockout', 'views/forms/base'], function ($, Back
 
         initialize: function() {
             BaseForm.prototype.initialize.apply(this);
+
             this.$el.find('.resource-type').select2({
                 placeholder: "type"
             });
 
-
-            this.NameModel = function(names) {
-                this.names = ko.observableArray(names);   
-            };
-
-            // attach the view models to the dom elements
-            this.nameViewModel = new this.NameModel(this.formdata.names);
-            ko.applyBindings(this.nameViewModel, $('#names-form')[0]);
+            this.viewModel.names = ko.observableArray(self.formdata.names);
         },
 
         addName: function(data) {
-            this.nameViewModel.names.push({
+            this.viewModel.names.push({
                 id: '',
                 name: $('#resource-name').val(),
                 type_id: $('#resource-name-type').val(),
@@ -36,13 +30,9 @@ define(['jquery', 'backbone', 'knockout', 'views/forms/base'], function ($, Back
         deleteName: function(el) {
             var data = $(el.target).data();
 
-            this.nameViewModel.names.remove(function(item) {
+            this.viewModel.names.remove(function(item) {
                 return item.name === data.id;
             });
-        },
-
-        getData: function(){
-            return ko.toJSON(this.nameViewModel);
         },
 
         validate: function(){
