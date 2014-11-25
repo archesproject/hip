ccbd29a8635a877fb65207cbba750db6b6948e38
Rob Gaston <rgaston@fargeo.com>
2014-11-25 15:49:42 -0800

    refactoring to use generic branch list js view

--- hip/media/js/forms/resource-summary-form.js
+++ hip/media/js/forms/resource-summary-form.js
@@ -1,13 +1,5 @@
-define(['jquery', 'backbone', 'knockout', 'knockout-mapping', 'underscore', 'views/forms/base'], function ($, Backbone, ko, koMapping, _, BaseForm) {
+define(['jquery', 'views/forms/base', 'views/forms/sections/branch-list'], function ($, BaseForm, BranchList) {
     return BaseForm.extend({
-
-        events: function(){
-            return _.extend(BaseForm.prototype.events.apply(this), {
-                'click #add-name-button': 'addName',
-                'click .arches-CRUD-delete ': 'deleteName'
-            }) 
-        },
-
         initialize: function() {
             var self = this;
 
@@ -15,33 +7,16 @@ define(['jquery', 'backbone', 'knockout', 'knockout-mapping', 'underscore', 'vie
 
             this.$el.find('.resource-type, .name-type').select2();
 
-            this.viewModel.NAME_E41 = ko.observableArray(this.viewModel.NAME_E41);
-
-            this.viewModel.editing.NAME_E41 = koMapping.fromJS(this.viewModel.defaults.NAME_E41);
+            new BranchList({
+                el: this.$el.find('#names-section')[0],
+                viewModel: this.viewModel,
+                key: 'NAME_E41',
+                pkField: 'NAME_E41__value'
+            })
 
             this.viewModel.editing.NAME_E41.NAME_TYPE_E55__value.subscribe(function(newValue) {
                 self.viewModel.editing.NAME_E41.NAME_TYPE_E55__label($('#resource-name-type option:selected').text());
             });
-        },
-
-        addName: function(data) {
-            var self = this;
-
-            this.viewModel.NAME_E41.push(ko.toJS(this.viewModel.editing.NAME_E41));
-            
-            koMapping.fromJS(this.viewModel.defaults.NAME_E41, this.viewModel.editing.NAME_E41);
-        },
-
-        deleteName: function(el) {
-            var data = $(el.target).data();
-
-            this.viewModel.NAME_E41.remove(function(item) {
-                return item.NAME_E41__value === data.name_e41__value;
-            });
-        },
-
-        validate: function(){
-            return true;
         }
     });
 });
