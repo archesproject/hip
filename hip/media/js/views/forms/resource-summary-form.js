5741847485c801661db9365545861a0874a2defe
Rob Gaston <rgaston@fargeo.com>
2014-11-26 11:31:10 -0800

    implementing simple branch validation for NAME_E41

--- hip/media/js/views/forms/resource-summary-form.js
+++ hip/media/js/views/forms/resource-summary-form.js
@@ -1,15 +1,19 @@
 define(['jquery', 'views/forms/base', 'views/forms/sections/branch-list'], function ($, BaseForm, BranchList) {
     return BaseForm.extend({
         initialize: function() {
-            var self = this;
+            var self = this,
+                nameBranchList;
 
             BaseForm.prototype.initialize.apply(this);
 
-            new BranchList({
+            nameBranchList = new BranchList({
                 el: this.$el.find('#names-section')[0],
                 viewModel: this.viewModel,
                 key: 'NAME_E41',
-                pkField: 'NAME_E41__entityid'
+                pkField: 'NAME_E41__entityid',
+                validateBranch: function (data) {
+                    return data.NAME_E41__value;
+                }
             });
         }
     });
