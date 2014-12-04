64a9427bbcd3fb483b937cefd73cce8e5ca6cc34
Rob Gaston <rgaston@fargeo.com>
2014-12-03 19:18:50 -0800

    quick cleanup/refactor

--- hip/media/js/views/forms/measurement-form.js
+++ hip/media/js/views/forms/measurement-form.js
@@ -6,25 +6,11 @@ define(['jquery', 'underscore', 'views/forms/base', 'views/forms/sections/branch
 
             BaseForm.prototype.initialize.apply(this);
 
-            nameBranchList = new BranchList({
+            this.branchLists.push(new BranchList({
                 el: this.$el.find('#measurement-section')[0],
                 viewModel: this.viewModel,
-                key: 'MEASUREMENT_TYPE_E55',
-                validateBranch: function (data) {
-                    // currently we are using the string 'Primary' as the key for the type... this is very brittle and should be enhanced
-                    var primaryLabel = 'Primary',
-                        valid = data.VALUE_OF_MEASUREMENT_E60__value ? true : false;
-                    if (data.MEASUREMENT_TYPE_E55__label === primaryLabel) {
-                        _.each(self.viewModel[nameBranchList.key](), function (item) {
-                            if (item.MEASUREMENT_TYPE_E55__label === primaryLabel) {
-                                valid = false;
-                            }
-                        });
-                    }
-                    return valid;
-                }
-            });
-            this.branchLists.push(nameBranchList);
+                key: 'MEASUREMENT_TYPE_E55'
+            }));
         }
     });
 });
