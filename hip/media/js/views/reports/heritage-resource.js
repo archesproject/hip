8c3ea06b73f163be460041883abbab8c341c1c07
chiatt <chiatt@fargeo.com>
2014-12-10 08:16:55 -0800

    Added data and layer switcher to map

--- hip/media/js/views/reports/heritage-resource.js
+++ hip/media/js/views/reports/heritage-resource.js
@@ -13,9 +13,7 @@ require(['jquery','arches','bootstrap','openlayers'], function($, arches) {
             el.attachEvent('onclick', handleArchesReport);
 
         function handleArchesReport() {    
-
             var panel = $('.arches-report-options');
-
             //get position of the Report options button, and use to place the panel
             //relative to the button
             var offset = $("#report_options").offset().top + $("#report_options").height();
@@ -26,8 +24,6 @@ require(['jquery','arches','bootstrap','openlayers'], function($, arches) {
             //side of the screen.  Subtract this from the total screen width to find the position relative 
             //to the right side of the screen
             var right_edge = $(window).width() - (left_edge +  $("#report_options").width());
-            
-            //alert("offset " + offset + " left edge " + left_edge + " right edge " + right_edge);
 
             //Display the panel
             $('.arches-report-options').show();
@@ -38,40 +34,71 @@ require(['jquery','arches','bootstrap','openlayers'], function($, arches) {
             });
         }
 
-        var styles = [
-                  'Aerial','Road','AerialWithLabels','collinsBart','ordnanceSurvey'
-                ];
+        function createVectorLayer(){
+            var format = new ol.format.WKT();
+            var feature = format.readFeature($('#map-content').val());
+            feature.getGeometry().transform('EPSG:4326', 'EPSG:3857');
+            var vector = new ol.layer.Vector({
+                  source: new ol.source.Vector({
+                    features: [feature],
+                    visible: true
+                  })
+                });
+            return vector
+            }
 
-        var layers = [];
-        var i, ii;
-        for (i = 0, ii = styles.length; i < ii; ++i) {
-          layers.push(new ol.layer.Tile({
-            visible: false,
-            preload: Infinity,
-            source: new ol.source.BingMaps({
-              key: 'Ak-dzM4wZjSqTlzveKz5u0d4IQ4bRzVI309GxmkgSVr1ewS6iPSrOvOKhA-CJlm3',
-              imagerySet: styles[i]
-            })
-          }));
+        function loadLayers(vector){
+            var basemaps = [
+                      'Road','Aerial'
+                    ];
+            var layers = [];
+            var i, ii;
+            for (i = 0, ii = basemaps.length; i < ii; ++i) {
+              layers.push(new ol.layer.Tile({
+                visible: false,
+                preload: Infinity,
+                source: new ol.source.BingMaps({
+                  key: 'Ak-dzM4wZjSqTlzveKz5u0d4IQ4bRzVI309GxmkgSVr1ewS6iPSrOvOKhA-CJlm3',
+                  imagerySet: basemaps[i]
+                })
+              }));
+            }
+
+            layers.push(vector);
+            return layers;
         }
+
+        function zoomToLayer(vectorLayer, map){
+            var extent = (vectorLayer.getSource().getExtent());
+            var size = (map.getSize());
+            view.fitExtent(
+                extent,
+                size
+              );
+            }
+
+        function switchLayer(){
+            var checkedLayer = $('#layerswitcher input[name=layer]:checked').val();
+            for (i = 0, ii = layers.length - 1; i < ii; ++i) layers[i].setVisible(i==checkedLayer);
+         }
+
+        var vectorLayer = createVectorLayer()
+        var layers = loadLayers(vectorLayer);
+
+        var view = new ol.View({
+            center: [-13168799.0, 4012635.2],
+            zoom: 10
+            })
+
         var map = new ol.Map({
           layers: layers,
           target: 'map',
-          view: new ol.View({
-                center: [-13168799.0, 4012635.2],
-                zoom: 10
-          })
+          view: view
         });
 
-        $('#layer-select').change(function() {
-          var style = $(this).find(':selected').val();
-          var i, ii;
-          for (i = 0, ii = layers.length; i < ii; ++i) {
-            layers[i].setVisible(styles[i] == style);
-          }
-        });
-        $('#layer-select').trigger('change');
-
+        zoomToLayer(vectorLayer, map)
+        $(function() { switchLayer() } );
+        $("#layerswitcher input[name=layer]").change(function() { switchLayer() } );
 
     });
 
