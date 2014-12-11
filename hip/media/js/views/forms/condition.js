148b4c1a78a231523a6a908f8e421e5510d23cc6
chiatt <chiatt@fargeo.com>
2014-12-11 13:19:58 -0800

    Added updated conditions with new wizard form

--- hip/media/js/views/forms/condition.js
+++ hip/media/js/views/forms/condition.js
@@ -1,12 +1,34 @@
 define(['jquery', 'underscore', 'views/forms/base', 'views/forms/sections/branch-list'], function ($, _, BaseForm, BranchList) {
+
     return BaseForm.extend({
+
         initialize: function() {
-			var nameBrachList;
+            var conditionTypeBranchList;
             var self = this;
+
+            //Show and hide Upload Wizard.  
+            $("#start-upload").click(function(){ handleWizard(); return false; });
+            $("#save-upload").click(function(){
+                handleWizard();
+                saveWizard(); 
+                return false; 
+            });
+            $("#cancel-upload").click(function(){ handleWizard(); return false; });
+
+            function handleWizard() {    
+                $( ["#related-files-list", "#upload-wizard"].join(",") ).toggle(300);
+                $( ["#cancel-upload","#save-upload","#start-upload"].join(",")  ).toggle();
+            }
+
+            function saveWizard() {    
+                $( ["#completed-evaluations"].join(",")  ).toggle(300);
+                $( ["#related-files"].join(",")  ).css("display", "block");
+                $( ["#no-evaluations", "#no-files"].join(",")  ).css("display", "none");
+            }
 
             BaseForm.prototype.initialize.apply(this);
 
-            nameBranchList = new BranchList({
+            conditionTypeBranchList = new BranchList({
                 el: this.$el.find('#condition-section')[0],
                 viewModel: this.viewModel,
                 key: 'CONDITION_TYPE_E55',
@@ -18,7 +40,7 @@ define(['jquery', 'underscore', 'views/forms/base', 'views/forms/sections/branch
                     return valid;
                 }
             });
-            this.branchLists.push(nameBranchList);
+            this.branchLists.push(conditionTypeBranchList);
         }
     });
 });
