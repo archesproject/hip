26c73cd1ff1a63e68e394b26a0c4cb6ddbf94fce
Alexei Peters <apeters@fargeo.com>
2015-01-28 12:33:21 -0800

    add missing parameter to method

--- hip/views/resources.py
+++ hip/views/resources.py
@@ -85,7 +85,7 @@ def report(request, resourceid):
                 temp[concept_label['_id']] = concept_label['_source']
             else: 
                 # the resource graph referenced a non-preferred label or a label not in our target language, so we need to get the right label
-                temp[concept_label['_id']] = get_preflabel_from_conceptid(concept_label['_source']['conceptid'])
+                temp[concept_label['_id']] = get_preflabel_from_conceptid(concept_label['_source']['conceptid'], lang)
 
     # replace the uuid's in the resource graph with their preferred and localized label                    
     def crawl_again(items):
