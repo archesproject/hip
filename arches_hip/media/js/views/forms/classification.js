78b0b5731cc6bb059838c38aa4340adf160aa9e3
Alexei Peters <apeters@fargeo.com>
2015-03-23 10:42:59 -0700

    initial work on classification form

--- arches_hip/media/js/views/forms/classification.js
+++ arches_hip/media/js/views/forms/classification.js
@@ -1,7 +1,26 @@
-define(['jquery', 'underscore', 'views/forms/base', 'views/forms/sections/branch-list'], function ($, _, BaseForm, BranchList) {
+define(['jquery', 
+    'underscore', 
+    'views/forms/base', 
+    'views/forms/sections/branch-list',
+    'bootstrap-datetimepicker'], function ($, _, BaseForm, BranchList) {
     return BaseForm.extend({
         initialize: function() {
             BaseForm.prototype.initialize.apply(this);
+
+            var date_picker = $('.datetimepicker').datetimepicker({pickTime: false});
+            date_picker.on('dp.change', function(evt){
+                $(this).find('input').trigger('change'); 
+            });
+
+            this.addBranchList(new BranchList({
+                el: this.$el.find('#classification-section')[0],
+                data: this.data,
+                dataKey: 'PHASE_TYPE_ASSIGNMENT.E17',
+                validateBranch: function (nodes) {
+                    return true;
+                    return this.validateHasValues(nodes);
+                }
+            }));
         }
     });
 });
